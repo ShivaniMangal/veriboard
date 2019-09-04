acd09911da8cd6050b2770150f12efc7acdb610d
Raghul <raghsiva@outlook.com>
2019-09-04 17:48:17 +0530

    Added post filtering functionality

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import './styles.css'
-import ReadMoreReact from 'read-more-react';
 import ReadMoreAndLess from 'react-read-more-less';
-import {Modal,Button,ButtonToolbar,Form} from 'react-bootstrap'
-import Newpost from './Newpost'
+import axios from 'axios'
 
 class Dashboard extends React.Component {
 
@@ -16,8 +13,20 @@ class Dashboard extends React.Component {
       ],
       temp_title: '',
       temp_content: '',
+      boards :[],
       show : false
     }
+  }
+
+  componentDidMount() {
+    axios.get('http://192.168.20.97:8080/showboard')
+        .then(response => {
+            const data = JSON.parse(JSON.stringify(response.data));
+            console.log(data)
+            this.setState({
+              boards : data
+            });
+        })
   }
 
   handleChange = (evt) => {
@@ -61,23 +70,26 @@ class Dashboard extends React.Component {
     <textarea placeholder = "What's on your mind" class = "form-control" name="temp_content" value={this.state.temp_content} onChange={this.handleChange} style = {{width : '35rem', height : '10rem'}}/>
     </center>
     <br /><br />
-    <button class="btn btn-outline-primary" onClick = {this.handleSubmit}>Post Discussion</button>
+    <button class="btn btn-outline-danger" onClick = {this.handleSubmit}>Post Discussion</button>
   </form> 
 
 
 
 
-    let list = this.state.posts.map(
+    let list = this.state.boards.map(
       i => {
-        return <div className="container" style = {{width : '60rem', padding: '2px 16px', backgroundColor : 'black'}}>
+        for(var j = 0; j < i.members.length ; j++){
+          console.log(i.members[j])
+        if(i.members[j] == "amal121"){  
+        return <div className="container" style = {{width : '60rem', padding: '2px 16px'}}>
           <hr />
           <div class="card" style = {{width : '50rem', align: 'center', rgba:'(0,0,0,0.2)',transition: '0.3s'}}>
             <div >
               <div >
                 <h4 class = "card-title">
-                  <center> {i.title}</center>
+                  <center> {i.topic}</center>
                   </h4>
-                <h6 class="card-subtitle mb-2 text-muted">Username</h6>
+                <h6 class="card-subtitle mb-2 text-muted">{i.creator}</h6>
                 <ReadMoreAndLess
                 ref={this.ReadMore}
                 className="read-more-content"
@@ -85,25 +97,29 @@ class Dashboard extends React.Component {
                 readMoreText="Read more"
                 readLessText="Read less"
                 >
-                 {i.content}
+                {i.posts[0].data}
                 </ReadMoreAndLess>
-                 <br /> <br />
-                 <button class="btn btn-outline-primary" >Open Discussion</button><br /> <br />&nbsp;&nbsp;
+                <br /> <br />
+                <button class="btn btn-outline-primary" >Open Discussion</button><br /> <br />&nbsp;&nbsp;
               </div> 
             </div>
           </div>
 
           <br />
         </div>
-
       }
+    }
+    }
     );
 
 
 
     return (
         <div className="container" style = {{padding: '2px 16px'}}>
-            <button class="btn btn-outline-primary center-block" name = "create" onClick = {this.createPost} style = {{float : 'left'}}>Create Post</button>  
+            <br />
+            <h3> Veriboard Discussion Forums</h3>
+            <h6 style={{color:"#CD040B"}}> Ask. Discuss. Learn.</h6> <br />
+            <button class="btn btn-outline-danger center-block" name = "create" onClick = {this.createPost} style = {{float : 'left'}}>Create Post</button>  
           <br/ > <br />
          {this.state.show?posts:<div></div>}
         <center>{list}</center>
