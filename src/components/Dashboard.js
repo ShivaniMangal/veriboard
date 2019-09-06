f772ff0f863184d5745d1108e2cec1fe3272650c
Raghul <raghsiva@outlook.com>
2019-09-06 16:04:16 +0530

    Added search for user filtering

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -17,7 +17,10 @@ class Dashboard extends React.Component {
       boardLength : 0,
       show : false,
       members : [],
-      temp_board : ''
+      temp_board : '',
+      usernames : [],
+      username :'',
+      names : ''
     }
   }
 
@@ -30,6 +33,14 @@ class Dashboard extends React.Component {
               boardLength : data.length + 1001 
             });
         })
+
+        axios.get('http://192.168.20.87:8003/register/usernames')
+        .then(response => {
+            const data = JSON.parse(JSON.stringify(response.data));
+            this.setState({
+                usernames: data
+            });
+        })    
   }
 
   componentDidUpdate() {
@@ -61,7 +72,7 @@ class Dashboard extends React.Component {
       flag : 'private',
       board_id : this.state.boardLength,
       topic : this.state.temp_title,
-      creator : this.state.temp_title,
+      creator : localStorage.getItem("username"),
       members : inputs,
       posts : [{
        postid : 1,
@@ -82,6 +93,31 @@ class Dashboard extends React.Component {
     event.preventDefault();
   }
 
+  handleChangeUser = (userN) => {
+    console.log(this.refs.username.value)
+    let postName = this.refs.username.value
+    let namesArr = []
+    this.setState({
+        names: []
+    })
+    for (var i = 0; i < this.state.usernames.length; i++) {
+        let flag = 0
+        for (var j = 0; j < postName.length; j++) {
+            if (this.state.usernames[i].name[j] !== postName[j]) { flag = 1; }
+        }
+        if (flag == 0) {
+            namesArr.push(this.state.usernames[i].name)
+            //console.log(namesArr)
+            this.forceUpdate()
+
+        }
+        // console.log(this.state.names)
+    }
+    this.setState({
+        names: namesArr
+    })
+}
+
   createPost = () => {
     this.setState({
       show:true
@@ -100,6 +136,13 @@ class Dashboard extends React.Component {
   }
 
   render() {
+    let userList = []
+        for(var i = 0; i<this.state.names.length;i++){
+            userList.push(
+                <div>{this.state.names[i]}</div>
+            )
+        }
+
     let posts =
     <form class = "form-group">  
     <center> 
@@ -109,9 +152,10 @@ class Dashboard extends React.Component {
     <textarea placeholder = "What's on your mind" class = "form-control" name="temp_content" value={this.state.temp_content} onChange={this.handleChange} style = {{width : '35rem', height : '10rem'}}/><br /> <br />
     </center>
     <center>
-    <input type = "text" placeholder = "Manage who can see your board" class = "form-control" name="temp_board" value={this.state.temp_board} onChange={this.handleChange} style = {{width : '35rem'}}/>
+    <input type = "text" placeholder = "Manage who can see your board" class = "form-control" ref="username" type="text" name="userN" onChange={this.handleChangeUser} style = {{width : '35rem'}}/>
     </center>
     <br /><br />
+    <center>{userList}</center>
     <button class="btn btn-outline-danger" onClick = {this.handleSubmit}>Post Discussion</button>
   </form> 
 
@@ -123,10 +167,10 @@ class Dashboard extends React.Component {
         if(i.members){
           let bid = i.board_id
         for(var j = 0; j < i.members.length ; j++){
-        if(i.members[j] == "anshbhar112"){
+        if(i.members[j] == localStorage.getItem("username") || i.creator == localStorage.getItem("username")){
         return <div className="container" style = {{width : '60rem', padding: '2px 16px'}}>
           <hr />
-          <div class="card" style = {{width : '50rem', align: 'center', rgba:'(0,0,0,0.2)',transition: '0.3s'}}>
+          <div class="card rounded" style = {{width : '50rem' ,align: 'center', rgba:'(0,0,0,0.2)',transition: '0.3s', borderWidth : 'medium'}}>
             <div >
               <div >
                 <h4 class = "card-title">
@@ -143,7 +187,7 @@ class Dashboard extends React.Component {
                 {i.posts[0].data}
                 </ReadMoreAndLess>
                 <br /> <br />
-                <button class="btn btn-outline-primary" onClick = {()=>this.openDiscussion(bid)}>Open Discussion</button><br /> <br />&nbsp;&nbsp;
+                <button class="btn" style = {{backgroundColor : '#008080', color : '#fff'}} onClick = {()=>this.openDiscussion(bid)}>Open Discussion</button><br />&nbsp;&nbsp;
                  <br /> <br />
 
               </div> 
@@ -156,12 +200,9 @@ class Dashboard extends React.Component {
     }});
 
 
-    let imgUrl = '..\download.png'
+    const image_url = 'C:/Users/verizon/Pictures/th4OUVO0N7.jpg';
 
     return (
-      <div class = "row" >
-        <div class = "column" style = {{ float: 'left', width: '25%', backgroundColor : '#cccccc'}} />
-        <div class = "column" style = {{ float: 'left', width: '50%'}}>
         <div className="container" style = {{padding: '2px 16px'}}>
             <br />
             <h3> Veriboard Discussion Forums</h3>
@@ -170,11 +211,18 @@ class Dashboard extends React.Component {
           <br/ > <br />
          {this.state.show?posts:<div></div>}
         <center>{list}</center>
-      </div>
-      </div>
-      <div class = "column" style = {{ float: 'left', width: '25%', backgroundColor : '#cccccc'}}/> 
-      </div>
-    )
+        </div>
+    )  
+      {/* </div>
+      //</div>
+      <div class = "column" style = {{ float: 'left', width: '25%'}}> </div>
+          // background: "#466368",
+          // background: "-webkit-linear-gradient(#648880, #293f50)",
+          // background:    "-moz-linear-gradient(#648880, #293f50)",
+          // background:         "linear-gradient(#648880, #293f50)"
+          // background: "#648880",
+          // background: "linear-gradient(to top, #000, #648880, #293f50)"
+      </div> */}
   }
 }
 export default Dashboard; 
