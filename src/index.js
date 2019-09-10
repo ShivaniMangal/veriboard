59b7d4f9609d0b3f939882e729635a530224d9f1
Varshithaa Balaji <varshi2298@gmail.com>
2019-09-10 03:09:34 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import rootReducer from './reducer';
+import { createStore, applyMiddleware} from "redux";
+import thunkMiddleware from 'redux-thunk'
 
 import {BrowserRouter} from 'react-router-dom';
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<Provider store={createStore(rootReducer, applyMiddleware(thunkMiddleware))}>
+<BrowserRouter>
+    <App />
+</BrowserRouter>
+</Provider>, document.getElementById('root'));
 
